f48d8b2545e5806256527cb189b64cf247175b9a
bennilic <43956705+bennilic@users.noreply.github.com>
2018-11-12 09:19:45 +0100

    a fresh start_1.0

--- angularjs-vorlage/app/components/map/uStates.js
+++ angularjs-vorlage/app/components/map/uStates.js
@@ -54,11 +54,17 @@
     ];
     var uStates={};
 
-    uStates.draw = function(id, data){
+    uStates.draw = function(id, data, toolTip){
         function mouseOver(d){
+            d3.select("#tooltip").transition().duration(200).style("opacity", .9);
+
+            d3.select("#tooltip").html(toolTip(d.n, data[d.id]))
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
         }
 
         function mouseOut(){
+            d3.select("#tooltip").transition().duration(500).style("opacity", 0);
         }
 
         d3.select(id).selectAll(".state")
